8334d3db0f7a7be6ca1de28d610b1d0e3f9a21b5
Celine Lieutaud <celine.lieutaud@ynov.com>
2021-04-17 23:31:10 +0200

    Page de détails d'un article
    Ajout de la page de détails d'un article sans style

--- blog_celine/src/Controller/ArticleController.php
+++ blog_celine/src/Controller/ArticleController.php
@@ -6,19 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArticleRepository;
+use App\Entity\Article;
 
 class ArticleController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function list(ArticleRepository $ripo)
+    public function list(ArticleRepository $repo)
     {
-        $articles = $ripo->findAll();
+        $articles = $repo->findAll();
 
         return $this->render('article/home.html.twig', [
             'articles' => $articles,
         ]);
     }
 
+    /**
+     * @Route("/article/{id}", name="show")
+     */
+    public function show(Article $article) {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
 }
